91fdb203140d3e368e76da9aafe1fc0aa1412264
fabgitlocal <fmiredin7@gmail.com>
2019-09-06 14:55:58 +0200

    create jpa wrapper repository of entity ModelType: brought with feature of JpaRepository -> PagingAndSortingRepository -> Crud  -> repository

---
+++ src/main/java/com/guitar/db/repository/ModelTypeJpaRepositoriy.java
@@ -0,0 +1,11 @@
+package com.guitar.db.repository;
+
+import com.guitar.db.model.ModelType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModelTypeJpaRepositoriy  extends JpaRepository<ModelType, Long> {
+
+
+
+
+}
