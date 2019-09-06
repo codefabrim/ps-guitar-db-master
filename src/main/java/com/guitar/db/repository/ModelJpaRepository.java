16a980fbfeef941e731816e7a6c45497c50a1c50
fabgitlocal <fmiredin7@gmail.com>
2019-09-06 15:54:48 +0200

    add JpaRepository feature via Interface injected in actual contract DAO

--- src/main/java/com/guitar/db/repository/ModelJpaRepository.java
+++ src/main/java/com/guitar/db/repository/ModelJpaRepository.java
@@ -1,13 +1,11 @@
 package com.guitar.db.repository;
 
-import com.guitar.db.model.ModelType;
+import com.guitar.db.model.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ModelTypeJpaRepositoriy  extends JpaRepository<ModelType, Long> {
-
-
+public interface ModelJpaRepository  extends JpaRepository<Model, Long> {
 
 
 }
