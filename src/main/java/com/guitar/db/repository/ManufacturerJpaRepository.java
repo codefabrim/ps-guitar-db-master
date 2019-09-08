3589e5c525948d02aee738b75e14955c7d5b164c
fabgitlocal <fmiredin7@gmail.com>
2019-09-08 15:09:46 +0200

    Query DSL: use keyword of "Before" and After, Beetween

--- src/main/java/com/guitar/db/repository/ManufacturerJpaRepository.java
+++ src/main/java/com/guitar/db/repository/ManufacturerJpaRepository.java
@@ -4,8 +4,14 @@ import com.guitar.db.model.Manufacturer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface ManufacturerJpaRepository  extends JpaRepository <Manufacturer, Long>  {
+
+    List<Manufacturer> findByFoundedDateBefore(Date d);
+
 
 
 }
--- src/main/java/com/guitar/db/repository/ManufacturerRepository.java
+++ src/main/java/com/guitar/db/repository/ManufacturerRepository.java
@@ -54,10 +54,7 @@ public class ManufacturerRepository {
 	 * Custom finder
 	 */
 	public List<Manufacturer> getManufacturersFoundedBeforeDate(Date date) {
-		@SuppressWarnings("unchecked")
-		List<Manufacturer> mans = entityManager
-				.createQuery("select m from Manufacturer m where m.foundedDate < :date")
-				.setParameter("date", date).getResultList();
+		List<Manufacturer> mans = manufacturerJpaRepository.findByFoundedDateBefore(date);
 		return mans;
 	}
 
